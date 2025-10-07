1b6e4708b72d4efc5c5b9afeaeaf8a6e77d2533a
redarasmy <red7mer@gmail.com>
2025-10-07 14:34:04 +0100

    add basic tests for product-form

---
+++ src/features/admin/products/components/tests/product-form.test.tsx
@@ -0,0 +1,40 @@
+import { describe, expect, it } from "vitest"
+import { setup } from "@/tests/test-utils"
+import ProductForm from "@/features/admin/products/components/product-form"
+
+describe("Product Form", () => {
+    it("Should render", () => {
+        const { render } = setup(
+            <ProductForm
+                buttonText="Add Product"
+                categories={[]}
+                description=""
+                isSubmitting={false}
+                onSubmit={async () => {}}
+                title="Create new product"
+                open={true}
+            />
+        )
+        expect(render.getByText("Add Product")).toBeInTheDocument()
+        expect(render.getByText("Create new product")).toBeInTheDocument()
+    })
+    it("Should render with dialog trigger child", () => {
+        const { render } = setup(
+            <ProductForm
+                buttonText="Save Product"
+                categories={[]}
+                description="Add a new product"
+                isSubmitting={false}
+                onSubmit={async () => {}}
+                title="Create Product"
+                open={false}
+                onOpenChange={() => {}}
+            >
+                <button>Open Form</button>
+            </ProductForm>
+        )
+
+        // The trigger is always visible
+        expect(render.getByText("Open Form")).toBeInTheDocument()
+    })
+})
