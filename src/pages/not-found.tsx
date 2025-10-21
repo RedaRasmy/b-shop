62b60769c6eb271ecdcf5810f41a7b4fdb8adaa3
redarasmy <red7mer@gmail.com>
2025-10-21 21:18:30 +0100

    fix not-found page

--- src/pages/not-found.tsx
+++ src/pages/not-found.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 
 export default function NotFoundPage() {
     return (
-        <div className="w-full h-full space-y-2 flex flex-col items-center justify-center">
+        <div className="w-full h-screen space-y-2 flex flex-col items-center justify-center">
             <h1 className="font-bold text-3xl">404</h1>
             <p className="text-xl mb-5 font-semibold">Page not found!</p>
             <Button>
