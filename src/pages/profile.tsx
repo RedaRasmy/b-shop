f0e0d59414cd0828b051203ca1caf50d51c59911
redarasmy <red7mer@gmail.com>
2025-10-26 22:06:50 +0100

    change profile tab state to url state : ?tab=something

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -2,13 +2,26 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import AddressesTab from "@/features/profile/components/addresses-tab"
 import ProfileTab from "@/features/profile/components/profile-tab"
 import SettingsTab from "@/features/profile/components/settings-tab"
+import { useSearchParams } from "react-router-dom"
 
 export default function ProfilePage() {
+    const [searchParams, setSearchParams] = useSearchParams()
+
+    const activeTab = searchParams.get("tab") || "profile"
+
+    const handleTabChange = (value: string) => {
+        setSearchParams({ tab: value })
+    }
     return (
         <div className="container mx-auto px-4 py-8 max-w-4xl">
             <h1 className="text-3xl font-bold mb-8">Account</h1>
 
-            <Tabs className="space-y-6" defaultValue="profile">
+            <Tabs
+                className="space-y-6"
+                defaultValue="profile"
+                value={activeTab}
+                onValueChange={handleTabChange}
+            >
                 <TabsList className="grid w-full grid-cols-4">
                     <TabsTrigger value="profile">Profile</TabsTrigger>
                     <TabsTrigger value="orders">Orders</TabsTrigger>
@@ -16,20 +29,16 @@ export default function ProfilePage() {
                     <TabsTrigger value="settings">Settings</TabsTrigger>
                 </TabsList>
 
-                {/* Profile Tab */}
                 <TabsContent value="profile">
                     <ProfileTab />
                 </TabsContent>
 
-                {/* Orders Tab */}
                 <TabsContent value="orders">orders</TabsContent>
 
-                {/* Addresses Tab */}
                 <TabsContent value="addresses">
                     <AddressesTab />
                 </TabsContent>
 
-                {/* Settings Tab */}
                 <TabsContent value="settings">
                     <SettingsTab />
                 </TabsContent>
