3a0690f66dc5e5255a834bd79c5a1889ccd0e608
redarasmy <red7mer@gmail.com>
2025-10-27 22:49:53 +0100

    remove serializeParams function

--- src/lib/query-keys.ts
+++ src/lib/query-keys.ts
@@ -1,7 +1,5 @@
 import type { Status, Prettify } from "@/types/global-types"
 
-type Params = Record<string, string | number | boolean>
-
 type BasicQuery = {
     search?: string
     sort?: string
@@ -35,36 +33,17 @@ export type AdminCategoriesQuery = Prettify<
     }
 >
 
-function serializeParams(params?: Params) {
-    if (!params) return null
-    const sorted = Object.keys(params)
-        .sort()
-        .reduce((acc, key) => {
-            acc[key] = params[key]
-            return acc
-        }, {} as Params)
-    return JSON.stringify(sorted)
-}
-
 export const queryKeys = {
     products: {
         base: ["products"],
-        customer: (params?: ProductsQuery) => [
-            "products",
-            "customer",
-            serializeParams(params),
-        ],
+        customer: (params?: ProductsQuery) => ["products", "customer", params],
         infinite: (params?: ProductsQuery) => [
             "products",
             "customer",
             "infinite",
-            serializeParams(params),
+            params,
         ],
-        admin: (params?: ProductsQuery) => [
-            "products",
-            "admin",
-            serializeParams(params),
-        ],
+        admin: (params?: ProductsQuery) => ["products", "admin", params],
         detail: (id: string | number) => ["products", "detail", id],
         related: (id?: string) => ["products", "related", id],
     },
@@ -73,13 +52,9 @@ export const queryKeys = {
         customer: (params?: CategoriesQuery) => [
             "categories",
             "customer",
-            serializeParams(params),
+            params,
         ],
-        admin: (params?: CategoriesQuery) => [
-            "categories",
-            "admin",
-            serializeParams(params),
-        ],
+        admin: (params?: CategoriesQuery) => ["categories", "admin", params],
         detail: (id: string | number) => ["categories", "detail", id],
         products: (categoryId: string) => [
             "categories",
@@ -87,10 +62,4 @@ export const queryKeys = {
             "products",
         ],
     },
-    // orders: {
-    //     base: ["orders"],
-    //     customer: () => ["orders", "customer"],
-    //     admin: () => ["orders", "admin"],
-    //     success: (token: string) => ["orders", "success", token],
-    // },
 }
