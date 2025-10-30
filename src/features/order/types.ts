7efa8f26d558c77bb23879a97a2275bc71ae87a9
redarasmy <red7mer@gmail.com>
2025-10-30 14:43:29 +0100

    add AdminOrder type

--- src/features/order/types.ts
+++ src/features/order/types.ts
@@ -1,3 +1,5 @@
+// Customer
+
 export type Order = {
     items: {
         productName: string
@@ -12,3 +14,28 @@ export type Order = {
 }
 
 export type SuccessfulOrder = { total: string; id: number }
+
+/// Admin
+
+export type AdminOrder = {
+    status: "pending" | "processing" | "shipped" | "completed" | "canceled"
+    name: string
+    id: number
+    createdAt: Date
+    updatedAt: Date
+    email: string
+    phone: string
+    customerId: string | null
+    city: string
+    postalCode: string
+    addressLine1: string
+    addressLine2: string | null
+    orderToken: string
+    total: string
+    items: {
+        id: string
+        productId: string
+        quantity: number
+        priceAtPurchase: string
+    }[]
+}
