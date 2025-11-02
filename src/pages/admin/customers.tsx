c169806e7888ee9a92ae551dc0252490a5b99e72
redarasmy <red7mer@gmail.com>
2025-11-02 10:22:33 +0100

    add admin/customers page header

--- src/pages/admin/customers.tsx
+++ src/pages/admin/customers.tsx
@@ -1,3 +1,21 @@
+import AdminPageHeader from "@/features/admin/components/page-header"
+
 export default function AdminCustomersPage() {
-    return <div>customers</div>
+    // const totalText = data?.total ? `(${data.total} orders)` : ""
+
+    return (
+        <div className="space-y-6 h-full flex flex-col">
+            <AdminPageHeader
+                title="Customers"
+                description={`Manage your customer relationships ${0}`}
+            />
+            {/* <FilterControls {...controls} />
+            <CustomersTable customers={customers} />
+            <PaginationControl
+                page={page}
+                setPage={setPage}
+                totalPages={data?.totalPages ?? 0}
+            /> */}
+        </div>
+    )
 }
