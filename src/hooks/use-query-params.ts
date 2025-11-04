1d64c0fa1514fbfcc6ee130d034bf90ed2b555f8
redarasmy <red7mer@gmail.com>
2025-11-04 23:50:09 +0100

    add useQueryParams2 ( new version of the hook )

--- src/hooks/use-query-params.ts
+++ src/hooks/use-query-params.ts
@@ -1,4 +1,4 @@
-import type { RequiredButUndefined } from "@/types/global-types"
+import type { Prettify, RequiredButUndefined } from "@/types/global-types"
 import { useCallback, useMemo } from "react"
 import { useSearchParams } from "react-router-dom"
 
@@ -68,4 +68,103 @@ export function useQueryParams<T extends QueryParams>(
 //     const debouncedQuery = useDebounce({
 //         state: query,
 //     })
+// }
+
+////////   version 2
+
+type ParamType = "string" | "number" | "boolean"
+
+type Param<T extends ParamType> = {
+    type: T
+    default?: ToType<T>
+}
+
+type ToType<T extends ParamType> = T extends "string"
+    ? string
+    : T extends "number"
+    ? number
+    : boolean
+
+type Params = Record<
+    string,
+    Param<"string"> | Param<"number"> | Param<"boolean">
+>
+
+type Query<P extends Params> = {
+    [K in keyof P]: "default" extends keyof P[K]
+        ? ToType<P[K]["type"]>
+        : ToType<P[K]["type"]> | undefined
+}
+
+export function useQueryParams2<P extends Params>(params: P) {
+    const [searchParams, setSearchParams] = useSearchParams()
+
+    const query = useMemo(() => {
+        const entries = Object.entries(params).map(([key, value]) => {
+            const urlValue = searchParams.get(key)
+            if (urlValue) {
+                // check types
+                if (value.type === "number") {
+                    const parsed = Number(urlValue)
+                    return [key, isNaN(parsed) ? value.default : parsed]
+                } else if (value.type === "boolean") {
+                    const parsed =
+                        urlValue === "false"
+                            ? false
+                            : urlValue === "true"
+                            ? true
+                            : value.default
+                    return [key, parsed]
+                } else {
+                    return [key, urlValue]
+                }
+            } else {
+                return [key, value.default]
+            }
+        })
+
+        return Object.fromEntries(entries) as Prettify<Query<P>>
+    }, [searchParams, params])
+
+    const setQuery = useCallback(
+        (updates: Prettify<Partial<Query<P>>>) => {
+            const next = new URLSearchParams(searchParams)
+            for (const [key, val] of Object.entries(updates)) {
+                if (val === undefined || val === "") {
+                    next.delete(key)
+                } else {
+                    next.set(key, String(val))
+                }
+            }
+            setSearchParams(next)
+        },
+        [searchParams, setSearchParams]
+    )
+
+    return [query, setQuery] as const
+}
+
+// test
+
+// const testParams = {
+//     search: {
+//         type: "string",
+//         default: "hello",
+//     },
+//     sort: {
+//         type: "string",
+//     },
+//     price: {
+//         type: "number",
+//     },
+// } as const satisfies Params
+
+// function Compo2() {
+//     const [query, setQuery] = useQueryParams2(testParams)
+
+//     const updates = {
+//         price : 54
+//     }
+
+//     setQuery(updates)
 // }
