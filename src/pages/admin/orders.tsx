a0fb050e22408fe00aafb398a1045605b4472488
redarasmy <red7mer@gmail.com>
2025-11-09 19:39:05 +0100

    use getOptions in orders page

--- src/pages/admin/orders.tsx
+++ src/pages/admin/orders.tsx
@@ -1,36 +1,37 @@
 import OrdersTable from "@/features/admin/categories/orders/components/orders-table"
 import Filters from "@/features/admin/components/filter-controls"
+import { getOptions } from "@/features/admin/components/filter-controls/get-options"
 import AdminPageHeader from "@/features/admin/components/page-header"
 import PaginationControl from "@/features/admin/components/pagination"
 import usePaginatedFilters from "@/features/admin/hooks/use-paginated-filters"
 import { useUpdateOrder } from "@/features/order/api/mutations"
 import { useAdminOrders } from "@/features/order/api/queries"
 
-const sortOptions = [
-    { label: "Customer", value: "name" },
-    { label: "Status", value: "status" },
-    { label: "Total", value: "total" },
-    { label: "Date", value: "createdAt" },
-] as const
-
-const filterOptions = [
-    {
-        label: "Status",
-        value: "status",
-        options: [
-            { label: "Pending", value: "pending" },
-            { label: "Processing", value: "processing" },
-            { label: "Shipped", value: "shipped" },
-            { label: "Completed", value: "completed" },
-            { label: "Canceled", value: "canceled" },
-        ],
-    },
-] as const
+const options = getOptions({
+    filter: [
+        {
+            label: "Status",
+            value: "status",
+            options: [
+                { label: "Pending", value: "pending" },
+                { label: "Processing", value: "processing" },
+                { label: "Shipped", value: "shipped" },
+                { label: "Completed", value: "completed" },
+                { label: "Canceled", value: "canceled" },
+            ],
+        },
+    ],
+    sort: [
+        { label: "Customer", value: "name" },
+        { label: "Status", value: "status" },
+        { label: "Total", value: "total", type: "number" },
+        { label: "Date", value: "createdAt", type: "date" },
+    ],
+})
 
 export default function AdminOrdersPage() {
     const { query, controls, page, setPage } = usePaginatedFilters({
-        filterOptions,
-        sortOptions,
+        ...options,
         defaultSort: "createdAt:desc",
         pageSize: 5,
     })
