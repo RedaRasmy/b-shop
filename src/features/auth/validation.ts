8d1a5a3f9b4472c6dbe456df672654ef6395c2b9
reda <red7mer@gmail.com>
2026-06-20 18:47:55 +0100

    feat: add EmailSchema

--- src/features/auth/validation.ts
+++ src/features/auth/validation.ts
@@ -2,12 +2,17 @@ import z from "zod"
 
 export const PasswordSchema = z
     .string("Password is required")
-    .min(1,"Password is required")
+    .min(1, "Password is required")
     .min(8, "Password must be at least 8 characters long")
     .max(100, "Password is too long")
 
+export const EmailSchema = z
+    .string()
+    .min(1, "Email is required")
+    .pipe(z.email())
+
 export const CredentialsSchema = z.object({
-    email: z.string().min(1, "Email is required").email(),
+    email: EmailSchema,
     password: PasswordSchema,
 })
 
