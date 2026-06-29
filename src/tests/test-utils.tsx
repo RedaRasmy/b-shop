4c0ec4b7a2595e695e4f46d3f641adf52763e266
reda <red7mer@gmail.com>
2026-06-29 21:42:50 +0100

    test: add redux Provider

--- src/tests/test-utils.tsx
+++ src/tests/test-utils.tsx
@@ -1,21 +1,25 @@
 /* eslint-disable react-refresh/only-export-components */
 import { AuthProvider } from "@/features/auth/components/auth-provider"
+import { store } from "@/redux/store"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { render } from "@testing-library/react"
 import userEvent from "@testing-library/user-event"
 import { NuqsAdapter } from "nuqs/adapters/react-router/v7"
 import type { ComponentType, PropsWithChildren } from "react"
+import { Provider } from "react-redux"
 import { BrowserRouter } from "react-router-dom"
 
 export function renderWithProviders(ui: React.ReactElement, options = {}) {
     const testQueryClient = new QueryClient()
     const wrapper = ({ children }: PropsWithChildren) => (
         <QueryClientProvider client={testQueryClient}>
-            <NuqsAdapter>
-                <BrowserRouter>
-                    <AuthProvider>{children}</AuthProvider>
-                </BrowserRouter>
-            </NuqsAdapter>
+            <Provider store={store}>
+                <NuqsAdapter>
+                    <BrowserRouter>
+                        <AuthProvider>{children}</AuthProvider>
+                    </BrowserRouter>
+                </NuqsAdapter>
+            </Provider>
         </QueryClientProvider>
     )
     return render(ui, {
